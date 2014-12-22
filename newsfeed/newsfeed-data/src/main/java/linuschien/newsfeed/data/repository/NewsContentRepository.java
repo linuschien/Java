513a49a13043fac4542fc72c8658bbcc513ffc95
linuschien <linus.chien@gmail.com>
2014-12-22 16:52:05 +0800

    add search sample

--- newsfeed/newsfeed-data/src/main/java/linuschien/newsfeed/data/repository/NewsContentRepository.java
+++ newsfeed/newsfeed-data/src/main/java/linuschien/newsfeed/data/repository/NewsContentRepository.java
@@ -1,11 +1,16 @@
 package linuschien.newsfeed.data.repository;
 
+import java.util.List;
+
 import linuschien.newsfeed.data.model.NewsContent;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "contents", path = "contents")
 public interface NewsContentRepository extends MongoRepository<NewsContent, String> {
+
+	List<NewsContent> findByTitle(@Param("title") String title);
 
 }
