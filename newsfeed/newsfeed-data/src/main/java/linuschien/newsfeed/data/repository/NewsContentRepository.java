d54c676e5466b8872f5da050ac042c2fb4aa47d1
linuschien <linus.chien@gmail.com>
2014-12-22 16:19:37 +0800

    config Spring Data Rest

--- newsfeed/newsfeed-data/src/main/java/linuschien/newsfeed/data/repository/NewsContentRepository.java
+++ newsfeed/newsfeed-data/src/main/java/linuschien/newsfeed/data/repository/NewsContentRepository.java
@@ -3,7 +3,9 @@ package linuschien.newsfeed.data.repository;
 import linuschien.newsfeed.data.model.NewsContent;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "content", path = "content")
 public interface NewsContentRepository extends MongoRepository<NewsContent, String> {
 
 }
---
+++ newsfeed/newsfeed-web/src/main/java/linuschien/newsfeed/web/WebApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package linuschien.newsfeed.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+@Configuration
+@Import(RepositoryRestMvcConfiguration.class)
+public class WebApplicationConfiguration {
+	
+}
