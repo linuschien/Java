29b6d1ba519be96ca50fd6d93081ce83faafdb1b
linuschien <linus.chien@gmail.com>
2014-12-22 17:39:40 +0800

    remove rel annotation

--- newsfeed/newsfeed-data/src/main/java/linuschien/newsfeed/data/repository/NewsContentRepository.java
+++ newsfeed/newsfeed-data/src/main/java/linuschien/newsfeed/data/repository/NewsContentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "contents", path = "contents")
+@RepositoryRestResource(path = "contents")
 public interface NewsContentRepository extends MongoRepository<NewsContent, String> {
 
 	List<NewsContent> findByTitle(@Param("title") String title);
