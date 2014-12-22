eaa7dfa87a3a6bbbfe202c568bf7063fe6927d11
linuschien <linus.chien@gmail.com>
2014-12-22 16:35:27 +0800

    change "content" to "contents"

--- newsfeed/newsfeed-data/src/main/java/linuschien/newsfeed/data/repository/NewsContentRepository.java
+++ newsfeed/newsfeed-data/src/main/java/linuschien/newsfeed/data/repository/NewsContentRepository.java
@@ -5,7 +5,7 @@ import linuschien.newsfeed.data.model.NewsContent;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "content", path = "content")
+@RepositoryRestResource(collectionResourceRel = "contents", path = "contents")
 public interface NewsContentRepository extends MongoRepository<NewsContent, String> {
 
 }
