1cda2ab04b77b9a47654770b7f72f3e3959fcc13
linuschien <linus.chien@gmail.com>
2014-12-23 12:35:33 +0800

    override default seetings

--- newsfeed/newsfeed-web/src/main/java/linuschien/newsfeed/web/WebApplicationConfiguration.java
+++ newsfeed/newsfeed-web/src/main/java/linuschien/newsfeed/web/WebApplicationConfiguration.java
@@ -1,11 +1,16 @@
 package linuschien.newsfeed.web;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @Configuration
-@Import(RepositoryRestMvcConfiguration.class)
-public class WebApplicationConfiguration {
-	
+public class WebApplicationConfiguration extends RepositoryRestMvcConfiguration {
+
+	@Override
+	protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.setBaseUri("/rest");
+		config.setDefaultPageSize(10);
+		config.setMaxPageSize(100);
+	}
 }
