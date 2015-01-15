a1549987e54239a1f48be981449d1e3e445d7c9c
linuschien <linus.chien@gmail.com>
2015-01-15 14:56:33 +0800

    refactor component test case

--- gmo-cao-integration-xmpp-component/src/test/java/com/gss/gmo/cao/integration/xmpp/component/ComponentTest.java
+++ gmo-cao-integration-xmpp-component/src/test/java/com/gss/gmo/cao/integration/xmpp/component/ComponentTest.java
@@ -1,23 +1,37 @@
 package com.gss.gmo.cao.integration.xmpp.component;
 
-import org.jivesoftware.whack.ExternalComponentManager;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.xmpp.component.AbstractComponent;
+import org.xmpp.component.Component;
 import org.xmpp.component.ComponentException;
+import org.xmpp.component.ComponentManager;
 import org.xmpp.packet.Message;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class ComponentTest {
+
+	private final String testMessage = "Test by Linus";
+	private String result;
+
+	@Autowired
+	private ComponentManager componentManager;
 
 	@Test
 	public void test() {
-		ExternalComponentManager componentManager = new ExternalComponentManager("im.gss.com.tw", 5275);
-		componentManager.setDefaultSecretKey("gss1234");
 		try {
-			componentManager.addComponent("ext", new AbstractComponent() {
+			Component component = new AbstractComponent() {
 
 				@Override
 				protected void handleMessage(Message message) {
 					log.info(message.getBody());
+					result = message.getBody();
 					log.info(getJID().toString());
 				}
 
@@ -30,15 +44,24 @@ public class ComponentTest {
 				public String getDescription() {
 					return "Linus External Component.";
 				}
-			});
+
+			};
+			componentManager.addComponent("ext", component);
+			Message message = new Message();
+			message.setTo("linus_chien@ext.im.gss.com.tw");
+			message.setBody(testMessage);
+			componentManager.sendPacket(component, message);
 		} catch (ComponentException e) {
 			e.printStackTrace();
 		}
+
 		try {
-			Thread.sleep(Integer.MAX_VALUE);
+			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+
+		assertEquals(testMessage, result);
 	}
 
 }
