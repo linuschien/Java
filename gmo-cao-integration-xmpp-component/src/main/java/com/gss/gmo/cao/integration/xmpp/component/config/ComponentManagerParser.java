6224924efcc011fd49ad929564f9b622040e76c3
linuschien <linus.chien@gmail.com>
2015-01-15 14:02:05 +0800

    add ComponentManagerParser

--- gmo-cao-integration-xmpp-component/src/main/java/com/gss/gmo/cao/integration/xmpp/component/config/ComponentManagerParser.java
+++ gmo-cao-integration-xmpp-component/src/main/java/com/gss/gmo/cao/integration/xmpp/component/config/ComponentManagerParser.java
@@ -1,7 +1,31 @@
 package com.gss.gmo.cao.integration.xmpp.component.config;
 
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.util.StringUtils;
+import org.w3c.dom.Element;
 
+/**
+ * @author linus_chien
+ *
+ */
 public class ComponentManagerParser extends AbstractSingleBeanDefinitionParser {
+
+	@Override
+	protected String getBeanClassName(Element element) {
+		return "org.jivesoftware.whack.ExternalComponentManager";
+	}
+
+	@Override
+	protected void doParse(Element element, BeanDefinitionBuilder builder) {
+		String host = element.getAttribute("host");
+		String port = element.getAttribute("port");
+		String defaultSecretKey = element.getAttribute("defaultSecretKey");
+		builder.addConstructorArgValue(host);
+		builder.addConstructorArgValue(port);
+		if (StringUtils.hasText(defaultSecretKey)) {
+			builder.addPropertyValue("defaultSecretKey", defaultSecretKey);
+		}
+	}
 
 }
