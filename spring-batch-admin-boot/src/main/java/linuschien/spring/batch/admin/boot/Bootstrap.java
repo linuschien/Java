e83e5f93d7b349fc3b8186c04b2d19be88efa0d3
Chien Cheng-Ming <linus.chien@gmail.com>
2015-05-15 16:32:09 +0800

    project which start spring-batch-admin by spring-boot

---
+++ spring-batch-admin-boot/src/main/java/linuschien/spring/batch/admin/boot/Bootstrap.java
@@ -0,0 +1,11 @@
+package linuschien.spring.batch.admin.boot;
+
+import org.springframework.boot.SpringApplication;
+
+public class Bootstrap {
+
+    public static void main(String[] args){
+	SpringApplication.run(SpringConfiguration.class, args);
+    }
+    
+}
